9932172aaea995d1775407c07681853a348db18a
bdlvhn <bdlvhn@outlook.com>
2022-04-21 19:40:59 +0900

    react start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  let post = "강남 우동 맛집!";
+  const [titles, setTitles] = useState([
+    "남자 코트 추천",
+    "여자 코트 추천",
+    "맛집 추천",
+  ]);
+  const [like, setLike] = useState(0);
+
+  const handleLike = () => {
+    setLike(like + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="black-nav">
+        <h4>Blog</h4>
+      </div>
+      <div className="list">
+        <h4>
+          {titles[0]} <span onClick={handleLike}>♥</span> {like}
+        </h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{titles[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{titles[2]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
